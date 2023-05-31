24092084f2b00af5996b21a775aa97413b6e595f
adover134 <45279353+adover134@users.noreply.github.com>
2023-05-31 17:35:24 +0900

    Update trainer.py 한글 주석 추가 중

--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ import torch
 
 from models.models import KnapsackExtractWeightsCostFromEmbeddingMLP, baseline_mlp_dict
 from models.modules import get_solver_module, StaticConstraintModule, CvxpyModule, CombOptNetModule
+# loss_from_string은 loss 이름을 문자열로 받아서 실행하는 메소드이다.
 from utils.utils import loss_from_string, optimizer_from_string, set_seed, AvgMeters, compute_metrics, \
     knapsack_round, compute_normalized_solution, compute_denormalized_solution, solve_unconstrained
 
